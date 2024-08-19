6c50ab1053d1de380de9b3b2045ec43568bcf661
akeenantexasfcs <akeenan@texasfcs.com>
2024-08-18 23:32:24 -0400

    Add files via upload

--- matrix_profiles.py
+++ matrix_profiles.py
@@ -121,13 +121,12 @@ def main():
                 st.write(f"Match {i} distance: {distance:.4f}")
 
             # Plotting
-            fig = plt.figure(figsize=(15, 30))
-            gs = GridSpec(4, 1, height_ratios=[1, 1, 1, 1], figure=fig)
+            fig = plt.figure(figsize=(15, 20))
+            gs = GridSpec(3, 1, height_ratios=[1, 1, 1], figure=fig)
 
             ax1 = fig.add_subplot(gs[0])
             ax2 = fig.add_subplot(gs[1])
             ax3 = fig.add_subplot(gs[2])
-            ax4 = fig.add_subplot(gs[3])
             
             # Plot 1: Original time series
             date_range = pd.date_range(start=subsequence.index[0], periods=len(subsequence), freq='D')
